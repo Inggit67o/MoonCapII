d61939748308ea52b2cc54f54e35eb1f540e8e81
MollyMoo <156624411+Inggit67o@users.noreply.github.com>
2026-03-03 03:44:27 +0800

    Update main.sol

--- main.sol
+++ main.sol
@@ -166,3 +166,87 @@ contract MoonCapII {
     constructor() {
         topCurator = address(0x8B7C9d2E4f6A1b3c5D7e9F0a2B4c6d8E0f1A3B5);
         feeCollector = address(0x3F1A5b9c2D4e6f8A0b2C4d6E8f0a1B3c5D7e9F1);
+        emergencyGuard = address(0xE4D6f8A0B2c4e6F8a1B3c5d7E9f0A2b4C6d8E0f2);
+        treasury = address(0xA1b3C5d7E9f0A2b4C6d8E0f1A3b5C7d9E1f3A5b7);
+        deployBlock = block.number;
+        globalFeeBps = 25;
+        cooldownBlocks = 12;
+
+        if (topCurator == address(0)) revert MC2_CuratorCannotBeZero();
+        tierCapWei[0] = 1_000 ether;
+        tierCapWei[1] = 5_000 ether;
+        tierCapWei[2] = 10_000 ether;
+        tierCapWei[3] = 25_000 ether;
+        tierCapWei[4] = 50_000 ether;
+        tierCapWei[5] = 100_000 ether;
+    }
+
+    // -------------------------------------------------------------------------
+    // MODIFIERS
+    // -------------------------------------------------------------------------
+
+    modifier onlyCurator() {
+        if (msg.sender != topCurator) revert MC2_NotCurator();
+        _;
+    }
+
+    modifier onlyEmergencyGuard() {
+        if (msg.sender != emergencyGuard) revert MC2_NotEmergencyGuard();
+        _;
+    }
+
+    modifier whenLatticeNotPaused() {
+        if (_latticePaused[MC2_LATTICE_NAMESPACE]) revert MC2_LatticePaused();
+        _;
+    }
+
+    modifier nonReentrant() {
+        if (_reentrancyLock != 0) revert MC2_ReentrantCall();
+        _reentrancyLock = 1;
+        _;
+        _reentrancyLock = 0;
+    }
+
+    // -------------------------------------------------------------------------
+    // POD LIFECYCLE
+    // -------------------------------------------------------------------------
+
+    function spawnPod(
+        bytes32 podId,
+        uint8 riskTier,
+        uint256 minStakeWei,
+        uint256 maxStakeWei,
+        uint256 performanceFeeBps,
+        uint256 managementFeeBps
+    ) external onlyCurator nonReentrant whenLatticeNotPaused returns (bool) {
+        if (podId == bytes32(0)) revert MC2_ZeroPod();
+        if (_pods[podId].exists) revert MC2_PodExists();
+        if (podCount >= MC2_MAX_PODS) revert MC2_MaxPodsReached();
+        if (_podCountByCurator[topCurator] >= MC2_MAX_PODS_PER_CURATOR) revert MC2_MaxPodsPerCuratorReached();
+        if (riskTier > MC2_MAX_RISK_TIER) revert MC2_InvalidRiskTier();
+        if (performanceFeeBps > MC2_PERFORMANCE_FEE_BPS_CAP) revert MC2_PerformanceFeeTooHigh();
+        if (managementFeeBps > MC2_MANAGEMENT_FEE_BPS_CAP) revert MC2_InvalidFeeBps();
+        if (minStakeWei == 0 && maxStakeWei > 0) revert MC2_InvalidMinStake();
+
+        _pods[podId] = PodState({
+            podId: podId,
+            curator: topCurator,
+            riskTier: riskTier,
+            totalStakeWei: 0,
+            minStakeWei: minStakeWei,
+            maxStakeWei: maxStakeWei,
+            performanceFeeBps: performanceFeeBps,
+            managementFeeBps: managementFeeBps,
+            lastFeeBlock: block.number,
+            createdAtBlock: block.number,
+            frozen: false,
+            exists: true
+        });
+        _podIds.push(podId);
+        podCount++;
+        _podIdsByCurator[topCurator].push(podId);
+        _podCountByCurator[topCurator]++;
+        tierPodCount[riskTier]++;
+        _maybeTakeSnapshot(podId, 0);
+
+        emit PodSpawned(podId, topCurator, riskTier, minStakeWei, block.number);
